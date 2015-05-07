6f2ea2dbe3db1f8e0eb9c4288ca12bbad2f1faac
Mohammad Jamal <md.jamalmohiuddin@gmail.com>
2015-05-07 16:47:12 +0530

    Test program to generate rsa key pair

---
+++ generate_rsa_key/common.h
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+#include <openssl/evp.h>
---
+++ generate_rsa_key/gen_priv.c
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+#include "common.h"
+
+#define MODULUS_SIZE 2048
+#define EXPONENT RSA_F4
+#define CALLBACK_FN NULL
+#define CALLBACK_ARG NULL
+
+
+void initOpenSSL(void)
+{
+	OpenSSL_add_all_algorithms();
+	ERR_load_crypto_strings();
+	SSL_load_error_strings();
+	SSL_library_init();
+}
+
+
+EVP_PKEY * generate_PrivateKey(void)
+{
+	
+	EVP_PKEY *pkey;
+	RSA *rsa;
+
+	/*Allocates an empty EVP_PKEY structure*/
+	pkey = EVP_PKEY_new();
+
+	/*Generates a Key Pair and returns in a newly allocated RSA structure*/
+	rsa = RSA_generate_key(MODULUS_SIZE, EXPONENT, CALLBACK_FN, CALLBACK_ARG);
+
+	/*If key generation fails , RSA_generate_key returns NULL*/
+
+	if (!rsa) {
+		printf("Error: %ld\n", ERR_get_error());
+		return NULL;
+	}
+
+	/*Assigns rsa to pkey*/
+	if (EVP_PKEY_assign_RSA(pkey, rsa) != 1) {
+		printf("Error:%ld\n", ERR_get_error());
+		return NULL;
+	}
+	return pkey;
+}
+
+int main(int argc, char *argv[])
+{
+	
+	/*EVP_PKEY is a structure used by openssl to store private keys*/
+	EVP_PKEY *root_pkey;
+
+	initOpenSSL();
+
+	/*Generate a private key for root CA*/
+	root_pkey = generate_PrivateKey();
+
+	/*Printing it on the stdout*/
+	PEM_write_PrivateKey(stdout, root_pkey, NULL, NULL, 0, 0, NULL);
+
+	return 0;
+}
